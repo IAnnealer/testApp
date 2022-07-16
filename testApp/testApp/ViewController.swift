4866020da11d95a55150962143b9f1cc299b3c05
Iannealer <duwjdtn@ajou.ac.kr>
2022-07-16 10:46:05 +0900

    프로젝트 생성

---
+++ testApp/testApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by Ian on 2022/07/16.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
