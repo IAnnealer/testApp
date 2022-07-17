0b4c0b15ae805ecb366c24e3de1e9c3aace30c1b
Iannealer <duwjdtn@ajou.ac.kr>
2022-07-18 00:43:03 +0900

    Feat: Presentation - Home ViewController, ViewModel 구현

--- testApp/testApp/Sources/Presentation/Home/ViewModels/HomeViewModel.swift
+++ testApp/testApp/Sources/Presentation/Home/ViewModels/HomeViewModel.swift
@@ -5,21 +5,108 @@
 //  Created by Ian on 2022/07/16.
 //
 
-import Foundation
+import RxSwift
 
 final class HomeViewModel {
+  enum Section: Int, CaseIterable {
+    case banner
+    case goods
+  }
+
+  // MARK: - Properties
+  private(set) var contentsResponse: ContentReponse?
+  private var favoriteItemList: [Item]? {
+    didSet {
+      favoriteItemIdList = favoriteItemList?.map { $0.id }
+    }
+  }
+
+  private(set) var favoriteItemIdList: [Int]?
+  private(set) var hasMoreContents: Bool = true
+  private let useCase: HomeUseCase
+
+  init(useCase: HomeUseCase) {
+    self.useCase = useCase
+  }
 }
 
 extension HomeViewModel: ViewModelType {
   struct Input {
-
+    let requestInitialContents: Observable<Void>
+    let requestExtraContents: Observable<Void>
+    let addFavoriteItem: Observable<Item>
+    let deleteFavoriteItem: Observable<Item>
   }
 
   struct Output {
-
+    let didReceiveInitialContents: Observable<Void>
+    let didReceiveExtraContents: Observable<Void>
+    let didChangeFavoriteItemStatus: Observable<Void>
   }
 
   func transform(_ input: Input) -> Output {
-    return .init()
+    let didReceiveInitialContents = input.requestInitialContents
+      .flatMapLatest(fetchInitialContents)
+
+    let didReceiveExtraContents = input.requestExtraContents
+      .flatMapLatest(fetchExtraContents)
+
+    let didAddFavoriteItem = input.addFavoriteItem
+      .flatMapLatest(addFavorItem(item:))
+
+    let didDeleteFavoriteItem = input.deleteFavoriteItem
+      .flatMapLatest(deleteFavoriteItem(item:))
+
+    return .init(
+      didReceiveInitialContents: didReceiveInitialContents,
+      didReceiveExtraContents: didReceiveExtraContents,
+      didChangeFavoriteItemStatus: .merge(didAddFavoriteItem, didDeleteFavoriteItem)
+    )
+  }
+}
+
+// MARK: - Private
+private extension HomeViewModel {
+  func fetchInitialContents() -> Observable<Void> {
+    return .combineLatest(useCase.fetchContents(), useCase.fetchFavoriteItem()) { [weak self] in
+      self?.contentsResponse = $0
+      self?.favoriteItemList = $1
+      self?.hasMoreContents = true
+    }
+    .map { _ in }
+  }
+
+  func fetchExtraContents() -> Observable<Void> {
+    guard let lastId = contentsResponse?.goods.last?.id else { return .empty() }
+
+    return useCase.fetchMoreGoods(lastId: lastId)
+      .filter { [weak self] goodsResponse in
+        if goodsResponse.goods.isEmpty {
+          self?.hasMoreContents = false
+          return false
+        } else {
+          return true
+        }
+      }
+      .do(onNext: { [weak self] in
+        self?.contentsResponse?.goods.append(contentsOf: $0.goods)
+      })
+      .map { _ in }
+  }
+
+  func addFavorItem(item: Item) -> Observable<Void> {
+    return useCase.addFavoriteItem(item: item)
+      .do(onNext: { [weak self] in
+        self?.favoriteItemList = $0
+      })
+      .map { _ in }
+  }
+
+  func deleteFavoriteItem(item: Item) -> Observable<Void> {
+    return useCase.deleteFavoriteItem(item: item)
+      .do(onNext: { [weak self] in
+        self?.favoriteItemList = $0
+      })
+      .map { _ in }
   }
 }
--- testApp/testApp/Sources/Presentation/Home/Views/HomeViewController.swift
+++ testApp/testApp/Sources/Presentation/Home/Views/HomeViewController.swift
@@ -20,7 +20,12 @@ final class HomeViewController: BaseViewController {
 
   // MARK: - Properties
   private weak var collectionView: UICollectionView!
+  private var refreshControl: UIRefreshControl!
   private let viewModel: HomeViewModel!
+
+  private let requestExtraTrigger: PublishSubject<Void> = .init()
+  private let addFavoriteItemSubject: PublishSubject<Item> = .init()
+  private let deleteFavoriteItemSubject: PublishSubject<Item> = .init()
 
   init(viewModel: HomeViewModel) {
     self.viewModel = viewModel
@@ -32,6 +37,7 @@ final class HomeViewController: BaseViewController {
     fatalError("init(coder:) has not been implemented")
   }
 
+  // MARK: - Override
   override func viewDidLoad() {
     super.viewDidLoad()
   }
@@ -41,15 +47,24 @@ final class HomeViewController: BaseViewController {
     
     navigationItem.title = Constant.navigationTitle
 
+    refreshControl = .init().then {
+      $0.tintColor = GlobalConstant.Color.accent
+    }
+
     let flowLayout: UICollectionViewFlowLayout = .init().then {
       $0.scrollDirection = .vertical
+      $0.minimumInteritemSpacing = 0
+      $0.minimumLineSpacing = 0
     }
 
     collectionView = .init(frame: .zero, collectionViewLayout: flowLayout).then {
-      $0.registerCellClass(cellType: BannerCell.self)
+      $0.registerCellClass(cellType: BannerListCell.self)
       $0.registerCellClass(cellType: GoodsCell.self)
-      $0.rx.setDelegate(self).disposed(by: disposeBag)
+      $0.dataSource = self
+      $0.delegate = self
+      $0.showsHorizontalScrollIndicator = false
       $0.backgroundColor = .clear
+      $0.refreshControl = refreshControl
       view.addSubview($0)
     }
   }
@@ -62,11 +77,129 @@ final class HomeViewController: BaseViewController {
   }
 
   override func bind() -> Disposable {
+    guard let tabBarController = tabBarController else { return Disposables.create() }
+
+    let requestInitialContents: Observable<Void> = .merge(
+      rx.viewWillAppear,
+      refreshControl.rx.controlEvent(.valueChanged).asObservable()
+    )
+
+    let input: HomeViewModel.Input = .init(
+      requestInitialContents: requestInitialContents,
+      requestExtraContents: requestExtraTrigger.asObservable(),
+      addFavoriteItem: addFavoriteItemSubject.asObservable(),
+      deleteFavoriteItem: deleteFavoriteItemSubject.asObservable()
+    )
+    let output: HomeViewModel.Output = viewModel.transform(input)
+
     return Disposables.create([
-      super.bind()
+      super.bind(),
+
+      tabBarController.rx.didSelect
+        .asDriver()
+        .drive(onNext: { [weak self] _ in
+          self?.collectionView.scrollToItem(
+            at: .init(item: 0, section: 0),
+            at: .top,
+            animated: true)
+        }),
+
+      output.didReceiveInitialContents
+        .asDriverSkipError()
+        .drive(onNext: { [weak self] in
+          self?.collectionView.reloadData()
+          self?.refreshControl.endRefreshing()
+        }),
+
+      output.didReceiveExtraContents
+        .asDriverSkipError()
+        .drive(onNext: { [weak self] in
+          self?.collectionView.reloadData()
+        }),
+
+      output.didChangeFavoriteItemStatus
+        .subscribe()
     ])
   }
 }
 
-// MARK: - UICollectionViewDelegate
-extension HomeViewController: UICollectionViewDelegate { }
+// MARK: - UICollectionViewDataSource, UICollectionViewDelegate
+extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+  typealias HomeSection = HomeViewModel.Section
+
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    HomeSection.allCases.count
+  }
+
+  func collectionView(_ collectionView: UICollectionView,
+                      numberOfItemsInSection section: Int) -> Int {
+    guard let section = HomeSection(rawValue: section) else { return .zero }
+
+    switch section {
+    case .banner:
+      return 1
+    case .goods:
+      return viewModel.contentsResponse?.goods.count ?? 0
+    }
+  }
+
+  func collectionView(_ collectionView: UICollectionView,
+                      cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let section = HomeSection(rawValue: indexPath.section) else { return .init(frame: .zero) }
+
+    let cell: UICollectionViewCell
+    switch section {
+    case .banner:
+      let bannerListCell = collectionView.dequeueReusableCell(cellType: BannerListCell.self,
+                                                              indexPath: indexPath)
+      bannerListCell.configure(banners: viewModel.contentsResponse?.banners)
+      cell = bannerListCell
+    case .goods:
+      guard let item = viewModel.contentsResponse?.goods[safe: indexPath.item] else {
+        return .init(frame: .zero)
+      }
+
+      let isFavorite: Bool = viewModel.favoriteItemIdList?.contains(item.id) ?? false
+      let goodscell = collectionView.dequeueReusableCell(cellType: GoodsCell.self,
+                                                         indexPath: indexPath)
+      goodscell.configure(
+        item: item,
+        isFavoriteItem: isFavorite,
+        addFavoriteTrigger: addFavoriteItemSubject.asObserver(),
+        deleteFavoriteTrigger: deleteFavoriteItemSubject.asObserver()
+      )
+      cell = goodscell
+    }
+
+    return cell
+  }
+
+  func collectionView(_ collectionView: UICollectionView,
+                      willDisplay cell: UICollectionViewCell,
+                      forItemAt indexPath: IndexPath) {
+    guard let goodsCount = viewModel.contentsResponse?.goods.count else { return }
+
+    if indexPath.item == goodsCount - 1 && viewModel.hasMoreContents {
+      requestExtraTrigger.onNext(())
+    }
+  }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      sizeForItemAt indexPath: IndexPath) -> CGSize {
+    guard let section = HomeSection(rawValue: indexPath.section) else { return .zero }
+
+    let width: CGFloat = collectionView.frame.width
+    switch section {
+    case .banner:
+      return .init(width: width, height: view.frame.height * 0.23)
+    case .goods:
+      guard let item = viewModel.contentsResponse?.goods[safe: indexPath.item] else { return .zero }
+      let defaultHeight: CGFloat = 60
+      return .calcuateGoodsCellSize(width: width, defaultHeight: defaultHeight, item: item)
+    }
+  }
+}
